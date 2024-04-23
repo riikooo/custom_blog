a654661b205bf4dbcf9c9dd17982bbea7fd081e9
rikooo <hikaru@example.com>
2024-04-23 10:12:02 +0900

    ルーター準備

--- custom_blog_react/src/App.tsx
+++ custom_blog_react/src/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { Header } from './components/Header'
 import Footer from './components/Footer'
 import { Main } from './components/Main'
 
 
+const router = createBrowserRouter([
+  {
+    path: "/main",
+    element: <Main/>,
+  },
+  // {
+  //   path: "/about",
+  //   element: <About/>,
+  // },
+]);
 
 function App() {
   return (
     <>
       <Header />
       <div style={{ marginBottom: '100px' }} />
-      <Main />
+      <RouterProvider router={router} />
       <div style={{ marginTop: '100px' }} />
       <Footer />
     </>
